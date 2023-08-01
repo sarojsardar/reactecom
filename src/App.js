260a7d68bbaa0b48c5aa52fb9fe59e6603e6bb01
unknown <sarojsardar25@gmail.com>
2023-08-01 17:02:07 +0545

    new changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Test1 from './Function';
 function App(){
   return(
     <div>
-      <Function text="This is the functional"/>
+      <Function text="This is  Test"/>
       <Test1 text="This is test1 function"/>
     </div>
   )
